80fa9dc87a8d659ace021a9baf447a3282d1469a
motie <50115514+M0tie@users.noreply.github.com>
2020-06-20 23:36:59 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ sudo chmod +x *.sh
 sudo mkdir /home/pi/scripts/
 cd
 sudo cp /home/pi/autolaunch/vita.service /etc/systemd/user/
-sudo cp /home/pi/autolaunch/91-vita.rule /etc/udev/rules.d/
-sudo cp /home/pi/autolaunch/92-dvita.rule /etc/udev/rules.d/
+sudo cp /home/pi/autolaunch/91-vita.rules /etc/udev/rules.d/
+sudo cp /home/pi/autolaunch/92-dvita.rules /etc/udev/rules.d/
 sudo cp /home/pi/autolaunch/close.sh /home/pi/scripts/
 sudo cp /home/pi/autolaunch/launch.sh /home/pi/scripts/
 sudo systemctl enable /etc/systemd/user/vita.service && sudo systemctl start vita.service
